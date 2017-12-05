776faf89abd21927e62b705994eff64662b236eb
Ellie Frymire <fryme970@newschool.edu>
2017-12-05 02:40:16 +0000

    update project 4

--- UNDP_4_Tools/halfcircles2.js
+++ UNDP_4_Tools/halfcircles2.js
@@ -1,3 +1,4 @@
+
 d3.csv("gender_calculation_gini.csv", function(error, data) {
     if (error) throw error;
     console.log(data);
@@ -8,9 +9,8 @@ function svg(data) {
     
     var groups = d3.select('div.content')
         .selectAll('div')
-        .data(data.sort(function(b, a){
-            return b["gii_rep"]-a["gii_rep"];
-            })).enter()
+        .data(data.filter(function(d){return d.region == 'Africa'}))
+        .enter()
         .append('div')
             .attr('class', function(d) { return 'col-md-2 col-sm-3 circle ' + d.country })
             .attr('width','150')
@@ -23,13 +23,13 @@ function svg(data) {
     div.append('svg')
         .attr('width','150')
         .attr('height','150')
-    
+        
     var svg = d3.selectAll("svg")
         svgWidth = svg.attr('width'),
         svgHeight = svg.attr('height')
         
     var tau = 2 * Math.PI;
-    
+        
     var gini = svg.append('path')
         .style("fill", "lightgrey")
         .attr('class', 'result')
@@ -40,7 +40,7 @@ function svg(data) {
             .endAngle(tau)
         )
         .attr('transform', 'translate(' + svgWidth/2 +  ',' + svgHeight/2 + ')')
-    
+        
     var female = svg.append('path')
         .style("fill", "red")
         .attr('class', 'result')
@@ -62,7 +62,7 @@ function svg(data) {
             .endAngle(tau)
         )
         .attr('transform', 'translate(' + svgWidth/2 +  ',' + svgHeight/2 + ')')
-    
+        
     var text = svg.append('text')
         .text(function(d) { return d.country} )
         .attr('width','100')
@@ -73,25 +73,46 @@ function svg(data) {
     d3.select("#sortMale")
 		.on("click", function() {
 			groups.sort(function(b, a){
-        return a["male_index"]-b["male_index"];
+            return a["male_index"]-b["male_index"];
         })})
 
     d3.select("#sortFemale")
 		.on("click", function() {
 			groups.sort(function(b, a){
-        return a["female_index"]-b["female_index"];
+            return a["female_index"]-b["female_index"];
         })})
         
     d3.select("#sortGii")
 		.on("click", function() {
 			groups.sort(function(b, a){
-        return a["gii_rep"]-b["gii_rep"];
+            return a["gii_rep"]-b["gii_rep"];
         })})
 
     d3.select("#sortGini")
 		.on("click", function() {
 			groups.sort(function(b, a){
-        return a["gini"]-b["gini"];
+            return a["gini"]-b["gini"];
         })})
-
+    
+//     d3.select("#filterAfrica")
+// 		.on("click", function() {
+// 		    groups.filter(function(d){return d.region == 'Africa'})
+// 		})
+		
+// 	d3.select("#filterEurope")
+// 		.on("click", function() {
+// 		    groups.filter(function(d){return d.region == 'Europe'})
+// 		})
+    
+//     d3.select("#filterAmericas")
+// 		.on("click", function() {
+// 		    groups.filter(function(d){return d.region == 'Americas'})
+// 		})
+		
+// 	d3.select("#filterAsia")
+// 		.on("click", function() {
+// 		    var region = 'Asia'
+// 		})
+		
 }
+
